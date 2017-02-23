95ca6f60e2ee8efb712c84f3cc921e050c8d5101
Yash Pandey <pandey.yash95@gmail.com>
2017-02-23 22:09:10 +0530

    Side navigation bar code in index.jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ronak', rollnumber: 'Test' });
+  res.render('index', { title: 'Yash Pandey', rollnumber: 'Test' });
 });
 
 module.exports = router;
